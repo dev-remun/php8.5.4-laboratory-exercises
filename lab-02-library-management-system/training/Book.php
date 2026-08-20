9b72500a5f678c91876a2e12c9fc755356bcabfc
dev-remun <raymond.cabarlo.balingbing@gmail.com>
2026-08-20 09:59:31 +0800

    lab2: Training

--- lab-02-library-management-system/training/Book.php
+++ lab-02-library-management-system/training/Book.php
@@ -13,6 +13,22 @@
             $this->book_published_date = $book_published_date;
         }
 
+        public function getBookTitle() {
+            return $this->book_title;
+        }
+
+        public function getBookAuthor() {
+            return $this->book_author;
+        }
+
+        public function getBookAbstract() {
+            return $this->book_abstract;
+        }
+
+        public function getBookPublishedDate() {
+            return $this->book_published_date;
+        }
+        
     }
 
 ?>
