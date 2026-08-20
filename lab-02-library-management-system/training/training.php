0ddc41aae452c393c9f5de2ceaad305283309fee
dev-remun <raymond.cabarlo.balingbing@gmail.com>
2026-08-20 10:01:15 +0800

    lab2: Training

--- lab-02-library-management-system/training/training.php
+++ lab-02-library-management-system/training/training.php
@@ -21,7 +21,7 @@
     $student_two = new Student("std2", "pass", 2, "BS Computer Science - 4A");
 
     $librarian_one = new Librarian("lib1@gmail.com", "pass", "College of Science", 1);
-    $librarian_two = new Librarian("librarian2@gmail.com", "password", "College of Nursing", 2);
+    $librarian_two = new Librarian("lib2@gmail.com", "pass", "College of Nursing", 2);
 
     $book_one = new Book("Book 1", "Lorem Ipsum", $sample_abstract, "02-02-2002");
     $book_two = new Book("Book 2", "Lorem Ipsum", $sample_abstract, "02-06-2002");
